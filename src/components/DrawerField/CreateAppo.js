4d00a0379b6b3e2eb60dd19885c88d082744fe16
shubham Hiwale <hiwale2144@gmail.com>
2022-03-07 14:27:32 +0530

    update side drawer inserting patient details comp. and add appont.

--- src/components/DrawerField/CreateAppo.js
+++ src/components/DrawerField/CreateAppo.js
@@ -1,7 +1,152 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Form,
+  Row,
+  Col,
+  Button,
+  InputGroup,
+  FormControl,
+  FloatingLabel,
+} from "react-bootstrap";
 
 const CreateAppo = () => {
-  return <div>Helo CreateAppo</div>;
+  const [appoData, setAppoData] = useState({
+    doctor_name: "",
+    email: "",
+    patient_name: "",
+    email: "",
+    mobile: "",
+    date: "",
+  });
+
+  return (
+    <>
+      <div className="position-relative dwer-container">
+        <Form>
+          <Col className="py-3">
+            <span className="font-weight-bold text-black">
+              Create An Appointment
+            </span>
+          </Col>
+          <Col className="">
+            <Form.Label>Choose doctor</Form.Label>
+            <InputGroup className="input-group-floting">
+              <InputGroup.Text>
+                <i class="fa fa-user-o" aria-hidden="true"></i>
+              </InputGroup.Text>
+              {/* <FloatingLabel label="Name"> */}
+              <Form.Select
+                type="text"
+                className="p-3"
+                placeholder="Name"
+                name="name"
+                value={appoData.doctor_name}
+              >
+                <option>demo 1</option>
+                <option>demo 2</option>
+                <option>demo 3</option>
+                <option>demo 4</option>
+                <option>demo 5</option>
+              </Form.Select>
+              {/* </FloatingLabel> */}
+            </InputGroup>
+          </Col>
+
+          <Col className="mt-4">
+            <Form.Label>Appointment Details</Form.Label>
+            <InputGroup className="input-group-floting">
+              <InputGroup.Text>
+                <i className="fa fa-user-o" aria-hidden="true"></i>
+              </InputGroup.Text>
+              <FloatingLabel label="Patient Name">
+                <Form.Control
+                  type="text"
+                  className="p-4"
+                  placeholder="Patient Name"
+                  name="patient_name"
+                  value={appoData.patient_name}
+                />
+              </FloatingLabel>
+            </InputGroup>
+          </Col>
+
+          <Col className="mt-4">
+            <InputGroup className="input-group-floting">
+              <InputGroup.Text>
+                <i className="fa fa-user-o" aria-hidden="true"></i>
+              </InputGroup.Text>
+              <FloatingLabel label="Email">
+                <Form.Control
+                  type="text"
+                  className="p-4"
+                  placeholder="Email"
+                  name="email"
+                  value={appoData.email}
+                />
+              </FloatingLabel>
+            </InputGroup>
+          </Col>
+
+          <Col className="mt-4">
+            <InputGroup className="input-group-floting">
+              <InputGroup.Text>
+                <i class="fa fa-user-o" aria-hidden="true"></i>
+              </InputGroup.Text>
+              <FloatingLabel label="Mobile Number">
+                <Form.Control
+                  type="text"
+                  className="p-4"
+                  placeholder="Mobile Number"
+                  name="mobile"
+                  value={appoData.mobile}
+                />
+              </FloatingLabel>
+            </InputGroup>
+          </Col>
+
+          <Col className="mt-4">
+            <InputGroup className="input-group-floting">
+              <InputGroup.Text>
+                <i class="fa fa-user-o" aria-hidden="true"></i>
+              </InputGroup.Text>
+              <FloatingLabel label="Date">
+                <Form.Control
+                  type="text"
+                  className="p-4"
+                  placeholder="Date"
+                  name="date"
+                  value={appoData.date}
+                />
+              </FloatingLabel>
+            </InputGroup>
+          </Col>
+
+          <Col className="mt-4">
+            <InputGroup className="input-group-floting">
+              <InputGroup.Text>
+                <i class="fa fa-user-o" aria-hidden="true"></i>
+              </InputGroup.Text>
+              <FloatingLabel label="Time">
+                <Form.Control
+                  type="text"
+                  className="p-4"
+                  placeholder="Time"
+                  name="time"
+                  value={appoData.time}
+                />
+              </FloatingLabel>
+            </InputGroup>
+          </Col>
+
+          <Col className="position-absolute fixed-bottom pb-2">
+            <Button className="w-100 p-3" variant="primary">
+              Create an Appointment
+            </Button>
+          </Col>
+        </Form>
+      </div>
+    </>
+  );
 };
 
 export default CreateAppo;
