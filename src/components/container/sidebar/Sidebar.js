633faf83e88728439d0bcdaf48417e83f88862dc
Nikhil Jain <njain923@gmail.com>
2022-03-07 12:37:27 +0530

    Update Sidebar.js

--- src/components/container/sidebar/Sidebar.js
+++ src/components/container/sidebar/Sidebar.js
@@ -3,11 +3,12 @@ import sidebarLogo from "../../../assets/img/hipaasafe-blue-logo.svg";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink, Route, Switch } from "react-router-dom";
 import appContext from "../../../context/appcontext/AppContext";
+import { useLocation } from 'react-router-dom';
+
 
 const Sidebar = () => {
-  const sbrAuth = useSelector((state) => state.sideBAuthReducer);
   const AppContext = useContext(appContext);
-
+  const location = useLocation();
   return (
     <>
       <ul
@@ -22,31 +23,31 @@ const Sidebar = () => {
             <img src={sidebarLogo} alt="Hipaasafe" className="sidebar-logo" />
           </div>
         </a>
-        <li className="nav-item active">
+        <li className={(location.pathname.includes('/main/support-dashboard') || location.pathname.includes('/main/dashboard')) ? "nav-item active" : "nav-item"}>
           <NavLink to={AppContext.user.role_id === 2 ? "/main/support-dashboard" : "/main/dashboard"} className="nav-link">
             <i class="fa fa-home" aria-hidden="true"></i>
             <span>Dashboard</span>
           </NavLink>
         </li>
-        {AppContext.user.role_id === 2 && <li className="nav-item">
+        {AppContext.user.role_id === 2 && <li className={(location.pathname.includes('/main/add-docter')) ? "nav-item active" : "nav-item"}>
           <NavLink to="/main/add-docter" className="nav-link">
             <i class="fa fa-user-md" aria-hidden="true"></i>
             <span>Add Doctors</span>
           </NavLink>
         </li>}
-        {AppContext.user.role_id === 2 && <li className="nav-item">
+        {AppContext.user.role_id === 2 && <li className={(location.pathname.includes('/main/add-nurse')) ? "nav-item active" : "nav-item"}>
           <NavLink to="/main/add-nurse" className="nav-link">
             <i class="fa fa-female" aria-hidden="true"></i>
             <span>Add Nurses</span>
           </NavLink>
         </li>}
-        {(AppContext.user.role_id === 3 || AppContext.user.role_id === 4) && <li className="nav-item">
+        {(AppContext.user.role_id === 3 || AppContext.user.role_id === 4) && <li className={(location.pathname.includes('/main/history')) ? "nav-item active" : "nav-item"}>
           <NavLink to="/main/history" className="nav-link">
             <i class="fa fa-clock-o" aria-hidden="true"></i>
             <span>History</span>
           </NavLink>
         </li>}
-        {(AppContext.user.role_id === 3 || AppContext.user.role_id === 4) && <li className="nav-item">
+        {(AppContext.user.role_id === 3 || AppContext.user.role_id === 4) && <li className={(location.pathname.includes('/main/schedule')) ? "nav-item active" : "nav-item"}>
           <NavLink to="/main/schedule" className="nav-link">
             <i class="fa fa-calendar" aria-hidden="true"></i>
             <span>Schedule</span>
